c3f51f76ed4c6402aad1ae2bc58e242b847843b4
Leo 🧙‍♂️ <leo.gisselberg@curbfood.com>
2022-09-29 14:10:40 +0200

    Revert

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,4 @@
-export const DATA_V1 = [
+export const DATA = [
   {
     id: 1,
   },
@@ -36,77 +36,6 @@ export const DATA_V1 = [
   },
   {
     id: 12,
-  },
-  {
-    id: 13,
-  },
-  {
-    id: 14,
-  },
-  {
-    id: 15,
-  },
-  {
-    id: 16,
-  },
-  {
-    id: 17,
-  },
-  {
-    id: 18,
-  },
-  {
-    id: 19,
-  },
-  {
-    id: 20,
-  },
-  {
-    id: 21,
-  },
-  {
-    id: 22,
-  },
-]
-
-export const DATA_V2 = [
-  {
-    id: 1,
-  },
-  {
-    id: 2,
-  },
-  {
-    id: 3,
-  },
-  {
-    id: 4,
-  },
-  {
-    id: 5,
-  },
-  {
-    id: 6,
-  },
-  {
-    id: 7,
-  },
-  {
-    id: 8,
-  },
-  {
-    id: 9,
-  },
-  {
-    id: 10,
-  },
-  {
-    id: 11,
-    isLarge: true
-  },
-  {
-    id: 12,
-    isLarge: true
   },
   {
     id: 13,
