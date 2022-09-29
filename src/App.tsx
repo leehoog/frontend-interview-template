21c400ab3e38aa80efc4a98504539f291355fcab
Leo 🧙‍♂️ <leo.gisselberg@curbfood.com>
2022-09-29 14:45:29 +0200

    Boilerplate html + css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className='grid'>
+        <div className='gridItem'>1</div>
+      </div>
     </div>
   );
 }
